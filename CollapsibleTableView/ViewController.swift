2772190e9d98a31f4c468fa26770644cb0452df7
Nick Rogers <mnicholasrogers@gmail.com>
2018-08-23 09:56:53 -0700

    Implemented example in ViewController.

--- CollapsibleTableView/ViewController.swift
+++ CollapsibleTableView/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView: UITableView?
+    var tableViewModel = CollapsibleTableViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height),
+                                style: .plain)
+        tableView?.dataSource = tableViewModel
+        tableView?.delegate = tableViewModel
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell_id")
+        tableView?.register(CollapsibleTableViewHeaderView.self, forHeaderFooterViewReuseIdentifier: "header_view")
+        tableView?.reloadData()
+        self.view.addSubview(tableView!)
+        
+        tableViewModel.reloadSection = { [weak self] (section) in
+            self?.tableView?.beginUpdates()
+            self?.tableView?.reloadSections([section], with: .fade)
+            self?.tableView?.endUpdates()
+        }
     }
 
     override func didReceiveMemoryWarning() {
